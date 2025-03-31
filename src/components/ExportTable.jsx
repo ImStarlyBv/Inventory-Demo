8c919a30a4b294f9dd7e0d3bb3280f91de716d52
Richardson Mercedes <alexander65mercedes@gmail.com>
2025-03-31 00:19:18 -0400

    feat(components): options component |

---
+++ src/components/ExportTable.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import * as XLSX from 'xlsx';
+
+const ExportTable = ({children, className}) => {
+  const now = new Date();
+    const currentDate = `${now.getDate()}-${now.getMonth() + 1}-${now.getFullYear()}-${now.getHours()}-${now.getMinutes()}-${now.getSeconds()}`;
+
+  const exportToExcel = () => {
+    const tabla = document.querySelector('.inventory-table');
+    const hoja = XLSX.utils.table_to_sheet(tabla);
+    const libro = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(libro, hoja, 'Datos');
+    XLSX.writeFile(libro, `inventario_${currentDate}.xlsx`);
+  };
+
+  return (
+    <button className={className} onClick={exportToExcel}>
+      {children}
+    </button>
+  );
+};
+
+export default ExportTable;
---
+++ src/components/Options.jsx
@@ -0,0 +1,40 @@
+import { useState } from "react"
+import DownloadIconReact from "@icons/DownloadIconReact"
+import AddIconReact from "@icons/AddIconReact"
+import DotsOptionsIconReact from "@icons/DotsOptionsIconReact"
+import ExportTable from "./ExportTable"
+
+const Options = () => {
+  const [showOptions, setShowOptions] = useState(false)
+
+  return (
+    <>
+      <div
+        className="border-2 border-black flex items-center rounded-md">
+        <button
+          className="p-2 cursor-pointer"
+          onClick={() => setShowOptions(!showOptions)}>
+          <DotsOptionsIconReact />
+        </button>
+      </div>
+
+      <div
+        className={`options absolute ${showOptions ? "top-[115%] opacity-100 visible" : "top-0 opacity-0 invisible"} duration-400 right-0 bg-white p-5 border-2 border-black rounded-2xl`}>
+        <button
+          className="bg-[#35D399] text-[#333333] py-2 px-4 rounded-lg cursor-pointer flex gap-3 items-center w-full"
+        >
+          <AddIconReact />
+          Añadir Producto
+        </button>
+        <ExportTable
+          className="border-2 border-gray-500 py-2 px-4 rounded-lg cursor-pointer flex gap-3 items-center whitespace-nowrap mt-2"
+        >
+          <DownloadIconReact />
+          Descargar inventario
+        </ExportTable>
+      </div>
+    </>
+  )
+}
+
+export default Options
